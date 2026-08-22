cf0c96dc2302dfecd08a8f13ef4561d9e8d2dc3f
niko <isadiewei@pm.me>
2026-08-22 19:20:33 -0400

    moved over the references for ai

--- code/sh/install.sh
+++ code/sh/install.sh
@@ -8,17 +8,18 @@ pip install pillow reportlab
 
 sudo apt-get install python3-tk
 # dont forget to add -y everywhere
-sudo apt install tmux -y
-sudo apt install python-is-python3
-sudo apt install -y tor
-sudo apt install -y openvpn
-sudo apt install -y ufw
-sudo apt install -y iptables
-sudo apt install -y curl
-sudo apt install -y dnsutils
-sudo apt install -y chipsec
-sudo apt install -y efibootmgr
-sudo apt install -y fwupd
-sudo apt install -y tpm2-tools
-sudo apt install -y pciutils
-sudo apt install -y dmidecode
+sudo apt-get install -y tmux
+sudo apt-get install -y python3-venv
+sudo apt-get install -y python-is-python3
+sudo apt-get install -y tor
+sudo apt-get install -y openvpn
+sudo apt-get install -y ufw
+sudo apt-get install -y iptables
+sudo apt-get install -y curl
+sudo apt-get install -y dnsutils
+sudo apt-get install -y chipsec
+sudo apt-get install -y efibootmgr
+sudo apt-get install -y fwupd
+sudo apt-get install -y tpm2-tools
+sudo apt-get install -y pciutils
+sudo apt-get install -y dmidecode
